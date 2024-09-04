0abe271e9ed1def6aa62f8eedcabb22c1da606a8
Oshada Perera <oshada.20222343@iit.ac.lk>
2024-09-04 21:42:51 +0530

    Added the Navbar, Language dropdown

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import "./navStyles.css";
+import LanDropDown from "../LanDropDown/LanDropDown";
+import Logo from "../../assets/Icons/SLTickets.svg";
+import { useTranslation } from "react-i18next";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const { t } = useTranslation();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="#" className="navbar-brand">
+          <img src={Logo} alt="SLTickets" className="logo" />
+        </a>
+        <div className="navbar-menu">
+          <a href="#" className="nav-link">
+            {t("nav.hostYourEvent")}
+          </a>
+          <a href="#" className="nav-link">
+            {t("nav.event")}
+          </a>
+          <a href="#" className="nav-link">
+            {t("nav.register")}
+          </a>
+          <a href="#" className="nav-link disabled">
+            {t("nav.signIn")}
+          </a>
+          <LanDropDown />
+        </div>
+        <div className="mobile-menu">
+          <button className="mobile-menu-button">
+            <svg
+              className="mobile-menu-icon"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
---
+++ src/pages/Event/Event.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./eventStyles.css";
+import NavBar from "../../components/NavBar/NavBar";
+
+function Event() {
+  return (
+    <div className="EventPage">
+      <NavBar />
+    </div>
+  );
+}
+
+export default Event;
